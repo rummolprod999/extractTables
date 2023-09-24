8828c371ee60438dd7e7bb0441114b8b60ff3b03
rummolprod999 <rummolprod999@gmail.com>
2023-09-24 16:08:57 +0300

    new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 import time
 from urllib.parse import unquote
+
 import requests
 
 
@@ -10,17 +10,15 @@ def print_hi(name):
         "Referer": "https://dl.twrp.me/gauguin/twrp-3.5.2_10-0-gauguin.img",
     }
     r = requests.get(
-        " http://zakupki.gov.ru/44fz/filestore/public/1.0/download/priz/file.html?uid=D3FC0FBCFE924423AA6EFDDD0AF182BB",
-        stream=True, headers=headers)
+            " http://zakupki.gov.ru/44fz/filestore/public/1.0/download/priz/file.html?uid=D3FC0FBCFE924423AA6EFDDD0AF182BB",
+            stream=True, headers=headers)
 
     ext = unquote(r.headers['Content-disposition'].split(';')[1].split('=')[1].strip('"'))
     with open(ext,
               'wb') as f:
-        for chunk in r.iter_content(1024):  # iterate on stream using 1KB packets
+        for chunk in r.iter_content(1024):
             f.write(chunk)
-
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-
