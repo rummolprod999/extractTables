3541ffb4e33d14da3c209843b246e8b9b6f3da5a
rummolprod999 <rummolprod999@gmail.com>
2023-09-24 19:42:49 +0300

    new

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
+import hashlib
+import os
+import sys
 import time
 from urllib.parse import unquote
 
+import tabula
 import requests
 
 
-def print_hi(name):
+def extract():
+    j = ''.join(sys.argv[1:]).encode('utf-8')
+    hash = hashlib.md5(j).hexdigest()
+    for f in sys.argv[1:]:
+        try:
+            getFile(f, hash)
+        except Exception as ex:
+            print(ex)
+
+
+def getFile(f, hash):
+   file, tmp = download(f, hash)
+   tables = tabula.read_pdf(file, pages="all")
+   print(tables)
+
+
+def download(f, hash):
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
         "Referer": "http://zakupki.gov.ru",
     }
     r = requests.get(
-            "http://zakupki.gov.ru/44fz/filestore/public/1.0/download/priz/file.html?uid=D3FC0FBCFE924423AA6EFDDD0AF182BB",
+            f,
             stream=True, headers=headers)
-
-    ext = unquote(r.headers['Content-disposition'].split(';')[1].split('=')[1].strip('"'))
-    with open(ext,
+    ext = list(filter(lambda o: o.find("filename=") != -1, r.headers['Content-disposition'].split(';')))
+    filename = unquote(ext[0].split('=')[1].strip('"'))
+    temp_file = f"{hash}/temp/{filename}"
+    filepath = os.path.join(hash, 'temp')
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    with open(temp_file,
               'wb') as f:
         for chunk in r.iter_content(1024):
             f.write(chunk)
+    return (os.path.abspath(temp_file), f"{hash}/temp")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    extract()
