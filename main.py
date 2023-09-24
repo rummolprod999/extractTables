47f6963c355107042a7bf2f12c05fbb3d308ecc2
rummolprod999 <rummolprod999@gmail.com>
2023-09-24 16:09:40 +0300

    new

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import requests
 def print_hi(name):
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
-        "Referer": "https://dl.twrp.me/gauguin/twrp-3.5.2_10-0-gauguin.img",
+        "Referer": "http://zakupki.gov.ru",
     }
     r = requests.get(
-            " http://zakupki.gov.ru/44fz/filestore/public/1.0/download/priz/file.html?uid=D3FC0FBCFE924423AA6EFDDD0AF182BB",
+            "http://zakupki.gov.ru/44fz/filestore/public/1.0/download/priz/file.html?uid=D3FC0FBCFE924423AA6EFDDD0AF182BB",
             stream=True, headers=headers)
 
     ext = unquote(r.headers['Content-disposition'].split(';')[1].split('=')[1].strip('"'))
