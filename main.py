eb98694ea7972f92a0dff9f3f70becb9e129b77a
rummolprod999 <rummolprod999@gmail.com>
2023-09-24 16:08:25 +0300

    new

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from urllib.parse import unquote
+import requests
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0",
+        "Referer": "https://dl.twrp.me/gauguin/twrp-3.5.2_10-0-gauguin.img",
+    }
+    r = requests.get(
+        " http://zakupki.gov.ru/44fz/filestore/public/1.0/download/priz/file.html?uid=D3FC0FBCFE924423AA6EFDDD0AF182BB",
+        stream=True, headers=headers)
+
+    ext = unquote(r.headers['Content-disposition'].split(';')[1].split('=')[1].strip('"'))
+    with open(ext,
+              'wb') as f:
+        for chunk in r.iter_content(1024):  # iterate on stream using 1KB packets
+            f.write(chunk)
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
