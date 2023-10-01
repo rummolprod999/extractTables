aa1442eec1fbe749ad223c2859d6df2d5bdfcfa2
rummolprod999 <rummolprod999@gmail.com>
2023-10-01 17:00:26 +0300

    new

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
+import glob
 import hashlib
+import json
 import os
+import shutil
 import sys
-import time
 from urllib.parse import unquote
 
-import tabula
 import requests
+import tabula
+from pyexcel import merge_all_to_a_book
+
+tmp = None
+list_file = []
 
 
 def extract():
     j = ''.join(sys.argv[1:]).encode('utf-8')
     hash = hashlib.md5(j).hexdigest()
+    tmp = f"{hash}/temp"
     for f in sys.argv[1:]:
         try:
             getFile(f, hash)
         except Exception as ex:
             print(ex)
+    shutil.rmtree(tmp)
+    print(list_file)
+    return json.dumps(list_file)
 
 
 def getFile(f, hash):
-    file, tmp = download(f, hash)
+    file = download(f, hash)
     tables = tabula.read_pdf(file, pages="all")
-    tabula.convert_into("reader_" + file, tmp + "output.csv", output_format="csv", pages="all")
-
-    print(tables)
+    csv_ = file + ".csv"
+    tabula.convert_into(file, csv_, output_format="csv", pages="all")
+    filename = file.replace("/temp", "") + ".xlsx"
+    merge_all_to_a_book(glob.glob(csv_), filename)
+    list_file.append(filename)
 
 
 def download(f, hash):
@@ -36,7 +48,7 @@ def download(f, hash):
             stream=True, headers=headers)
     ext = list(filter(lambda o: o.find("filename=") != -1, r.headers['Content-disposition'].split(';')))
     filename = unquote(ext[0].split('=')[1].strip('"'))
-    temp_file = f"{hash}/temp/{filename}"
+    temp_file = f"{hash}/temp/reader_{filename}"
     filepath = os.path.join(hash, 'temp')
     if not os.path.exists(filepath):
         os.makedirs(filepath)
@@ -44,7 +56,7 @@ def download(f, hash):
               'wb') as f:
         for chunk in r.iter_content(1024):
             f.write(chunk)
-    return os.path.abspath(temp_file), f"{hash}/temp"
+    return os.path.abspath(temp_file)
 
 
 if __name__ == '__main__':
